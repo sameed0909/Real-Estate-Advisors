9b8270f7749cb435e8d1121c98d7fb929a878e5a
Faisal Irfan <faisalirfan619@gmail.com>
2024-08-27 12:07:17 +0500

    Testing

--- src/Components/CoreFeatures.js
+++ src/Components/CoreFeatures.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const CoreFeatures = () => {
   return (
     <div>
-      
+      <h1>hello</h1>
     </div>
-  )
-}
+  );
+};
 
-export default CoreFeatures
+export default CoreFeatures;
