c9cf0619a5b529d7f7569ee25258e913ba6bf76c
Faisal Irfan <faisalirfan619@gmail.com>
2024-08-27 16:58:01 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-<<<<<<< HEAD
-import React from 'react';
-import Service from './Components/Service.js'
-import TestimonialsSection from './Components/TestimonialsSection.js';
-import AdisorsSection from './Components/AdvisorsSection.js'
-import HeroSection from './Components/HeroSection.js';
-
+import React from "react";
+import Service from "./Components/Service.js";
+import TestimonialsSection from "./Components/TestimonialsSection.js";
+import AdisorsSection from "./Components/AdvisorsSection.js";
+import HeroSection from "./Components/HeroSection.js";
 
 function App() {
   return (
     <div className="App">
-      <HeroSection/>
-      <Service/>
-      <TestimonialsSection/>
-      <AdisorsSection/>
-=======
-import './App.css';
-import HeroSection from './Components/HeroSection';
-import FeatureSection from './Components/FeatureSection';
-import Footer from './Components/Footer';
-function App() {
-  return (
-    <div className="App">
-            <HeroSection />
-<FeatureSection />
-<Footer />
->>>>>>> 79463ab2dc1982cd999108f59cf5919b3661f136
+      <HeroSection />
+      <FeatureSection />
+      <Service />
+      <TestimonialsSection />
+      <AdisorsSection />
+      <Footer />
     </div>
   );
 }
