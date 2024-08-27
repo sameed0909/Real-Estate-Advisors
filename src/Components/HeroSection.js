920f06bbdf9a105908e236a010c3bd3e0fe31394
Faisal Irfan <faisalirfan619@gmail.com>
2024-08-27 17:26:06 +0500

    fixed the hero section image

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -95,7 +95,7 @@ const HeroSection = () => {
         </div>
 
         {/* Right Section */}
-        <div className="md:w-1/2 h-full relative md:absolute top-0 right-0 w-full md:w-3/5 md:h-full z-0">
+        <div className="md:w-2/5 h-full relative md:absolute top-0 right-0 w-full z-0">
           <img
             src={`${process.env.PUBLIC_URL}/hero.png`}
             alt="Modern House"
