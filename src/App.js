6c16ed045beccc5de7ac59034f5c1c647f959ca0
sameed0909 <sameed1999.ahmed@gmail.com>
2024-08-27 17:03:55 +0500

    index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 import React from 'react';
 import Service from './Components/Service.js'
 import TestimonialsSection from './Components/TestimonialsSection.js';
 import AdisorsSection from './Components/AdvisorsSection.js'
 import HeroSection from './Components/HeroSection.js';
+import Footer from './Components/Footer.js';
 
 
 function App() {
@@ -13,18 +13,7 @@ function App() {
       <Service/>
       <TestimonialsSection/>
       <AdisorsSection/>
-=======
-import './App.css';
-import HeroSection from './Components/HeroSection';
-import FeatureSection from './Components/FeatureSection';
-import Footer from './Components/Footer';
-function App() {
-  return (
-    <div className="App">
-            <HeroSection />
-<FeatureSection />
-<Footer />
->>>>>>> 79463ab2dc1982cd999108f59cf5919b3661f136
+      <Footer/>
     </div>
   );
 }
